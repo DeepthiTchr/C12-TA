389320e94b181a175410c72b6273a723f5084ed3
Deepthi <70970105+DeepthiTchr@users.noreply.github.com>
2021-08-03 16:11:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,35 +9,35 @@ function preload() {
 }
 
 function setup() {
-createCanvas(600, 200);
+  createCanvas(600, 200);
 
-//create a trex sprite
-trex = createSprite(50,160,20,50);
-trex.addAnimation("running", trex_running);
-trex.scale = 0.5;
-  
-//create a ground sprite
-ground = createSprite(200,180,400,20);
-ground.addImage("ground",groundImage);
-ground.x = ground.width /2;
-ground.velocityX = -4;
+  //create a trex sprite
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+
+  //create a ground sprite
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
   
 }
 
 function draw() {
-background(220);
+  background(220);
 
-//jump when the space button is pressed
-if (keyDown("space")) {
-  trex.velocityY = -10;
-}
+  //jump when the space button is pressed
+  if (keyDown("space")) {
+    trex.velocityY = -10;
+  }
 
-trex.velocityY = trex.velocityY + 0.8
+  trex.velocityY = trex.velocityY + 0.8
 
-if (ground.x < 0) {
-  ground.x = ground.width / 2;
-}
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
 
-trex.collide(ground);
-drawSprites();
+  trex.collide(ground);
+  drawSprites();
 }
